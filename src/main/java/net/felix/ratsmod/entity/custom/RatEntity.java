452a1c819d805c86112679a7704a8f933e2016f3
Felix <felix.schumacher95@web.de>
2023-10-26 10:28:52 +0200

    tutorial: rat tamable

--- src/main/java/net/felix/ratsmod/entity/custom/RatEntity.java
+++ src/main/java/net/felix/ratsmod/entity/custom/RatEntity.java
@@ -2,9 +2,15 @@ package net.felix.ratsmod.entity.custom;
 
 import net.felix.ratsmod.entity.ModEntityTypes;
 import net.felix.ratsmod.item.ModItems;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
@@ -14,16 +20,15 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.OwnerHurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.OwnerHurtTargetGoal;
-import net.minecraft.world.entity.animal.Animal;
-import net.minecraft.world.entity.animal.Cow;
-import net.minecraft.world.entity.animal.Rabbit;
-import net.minecraft.world.entity.animal.Wolf;
-import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.scores.Team;
+import net.minecraftforge.event.ForgeEventFactory;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -36,64 +41,49 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 public class RatEntity extends TamableAnimal implements IAnimatable {
     private AnimationFactory factory = new AnimationFactory(this);
 
-    public RatEntity(EntityType<? extends RatEntity> pEntityType, Level pLevel) {
+    private static final EntityDataAccessor<Boolean> SITTING =
+            SynchedEntityData.defineId(RatEntity.class, EntityDataSerializers.BOOLEAN);
+
+    public RatEntity(EntityType<? extends TamableAnimal> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
         this.setTame(false);
     }
 
     public static AttributeSupplier createAttributes() {
-        return Animal.createLivingAttributes()
+        return TamableAnimal.createMobAttributes()
                 .add(Attributes.MAX_HEALTH, 5.0D)
-                .add(Attributes.FOLLOW_RANGE, 32.0D)
-                .add(Attributes.ATTACK_DAMAGE, 1.0D)
-                .add(Attributes.MOVEMENT_SPEED, 0.3F)
+                .add(Attributes.ATTACK_DAMAGE, 0.1D)
+                .add(Attributes.FOLLOW_RANGE, 32)
+                .add(Attributes.MOVEMENT_SPEED, 0.5D)
                 .build();
     }
-    //return MobEntity.createMobAttributes()
-    // .add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 0.0D)
-    // .add(StepHeightEntityAttributeMain.STEP_HEIGHT, 2.0D)
-    // .add(EntityAttributes.GENERIC_MAX_HEALTH, 8.0D)
-    // .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.5D)
-    // .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 0.1)
-    // .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 32);
 
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(1, new FloatGoal(this));
-        this.goalSelector.addGoal(2, new BreedGoal(this, 0.8D));
+        this.goalSelector.addGoal(1, new SitWhenOrderedToGoal(this));
+        this.goalSelector.addGoal(2, new PanicGoal(this, 1.25D));
         this.goalSelector.addGoal(3, new TemptGoal(this, 1.0D, Ingredient.of(
                 Items.CARROT, Items.APPLE, Blocks.DANDELION, ModItems.CHEESE.get()), false));
-        this.goalSelector.addGoal(5, new FollowParentGoal(this, 1.1D));
+        this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.1D));
         this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class, 10.0F));
+        this.goalSelector.addGoal(5, new FollowOwnerGoal(this, 1.0D, 10.0F, 2.0F, false));
         this.goalSelector.addGoal(6, new WaterAvoidingRandomStrollGoal(this, 0.6D));
-        this.goalSelector.addGoal(6, new FollowOwnerGoal(this, 1.0D, 10.0F, 2.0F, false));
-        this.goalSelector.addGoal(7, new BreedGoal(this, 1.0D));
         this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
 
         this.targetSelector.addGoal(1, new OwnerHurtByTargetGoal(this));
         this.targetSelector.addGoal(2, new OwnerHurtTargetGoal(this));
 
-        /*
-        Goals from Ratsmischief - find equivalent for these fabric goals
-        this.goalSelector.addGoal(1, new SwimGoal(this));
-        this.goalSelector.addGoal(2, new SitGoal(this));
-        this.goalSelector.addGoal(3, new EatToHealGoal(this));
-        this.goalSelector.addGoal(3, new PounceAtTargetGoal(this, 0.3F));
-        this.goalSelector.addGoal(4, new RatMeleeAttackGoal(this, 1.0D, true));
-        this.goalSelector.addGoal(5, new PickupItemGoal());
-        this.goalSelector.addGoal(5, new BringItemToOwnerGoal(this, 1.0D, false));
-        this.goalSelector.addGoal(6, new FollowOwnerRatGoal(this, 1.0D, 20.0F, 2.0F, false));
-        this.goalSelector.addGoal(8, new WanderAroundFarGoal(this, 1.0D));
-        this.targetSelector.addGoal(3, new RevengeGoal(this).setGroupRevenge());
-        this.targetSelector.addGoal(4, new TargetGoal<>(this, PlayerEntity.class, 10, true, false, this::shouldAngerAt));
-        this.targetSelector.addGoal(8, new ChaseForFunGoal<>(this, CatEntity.class, true));
-        this.targetSelector.addGoal(8, new UniversalAngerGoal<>(this, true));
-         */
     }
 
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
         if (event.isMoving()) {
             event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.rat.run", true));
+            return PlayState.CONTINUE;
+        }
+
+        if (isSitting()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.rat.flat", true));
             return PlayState.CONTINUE;
         }
 
@@ -133,4 +123,97 @@ public class RatEntity extends TamableAnimal implements IAnimatable {
         return 0.2F;
     }
 
+    /* TAMEABLE */
+    @Override
+    public InteractionResult mobInteract(Player player, InteractionHand hand) {
+        ItemStack itemstack = player.getItemInHand(hand);
+        Item item = itemstack.getItem();
+
+        Item itemForTaming = ModItems.CHEESE.get();
+
+        if (item == itemForTaming && !isTame()) {
+            if (this.level.isClientSide) {
+                return InteractionResult.CONSUME;
+            } else {
+                if (!player.getAbilities().instabuild) {
+                    itemstack.shrink(1);
+                }
+
+                if (!ForgeEventFactory.onAnimalTame(this, player)) {
+                    if (!this.level.isClientSide) {
+                        super.tame(player);
+                        this.navigation.recomputePath();
+                        this.setTarget(null);
+                        this.level.broadcastEntityEvent(this, (byte) 7);
+                        setSitting(true);
+                    }
+                }
+
+                return InteractionResult.SUCCESS;
+            }
+        }
+
+        if (isTame() && !this.level.isClientSide && hand == InteractionHand.MAIN_HAND) {
+            setSitting(!isSitting());
+            return InteractionResult.SUCCESS;
+        }
+
+        if (itemstack.getItem() == itemForTaming) {
+            return InteractionResult.PASS;
+        }
+
+        return super.mobInteract(player, hand);
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundTag tag) {
+        super.readAdditionalSaveData(tag);
+        setSitting(tag.getBoolean("isSitting"));
+    }
+
+    @Override
+    public void addAdditionalSaveData(CompoundTag tag) {
+        super.addAdditionalSaveData(tag);
+        tag.putBoolean("isSitting", this.isSitting());
+    }
+
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(SITTING, false);
+    }
+
+    public void setSitting(boolean sitting) {
+        this.entityData.set(SITTING, sitting);
+        this.setOrderedToSit(sitting);
+    }
+
+    public boolean isSitting() {
+        return this.entityData.get(SITTING);
+    }
+
+
+    @Override
+    public Team getTeam() {
+        return super.getTeam();
+    }
+
+    @Override
+    public boolean canBeLeashed(Player pPlayer) {
+        return false;
+    }
+
+    @Override
+    public void setTame(boolean pTamed) {
+        super.setTame(pTamed);
+        if (pTamed) {
+            getAttribute(Attributes.MAX_HEALTH).setBaseValue(10.0D);
+            getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(0.2D);
+            getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(1.0D);
+        } else {
+            getAttribute(Attributes.MAX_HEALTH).setBaseValue(5.0D);
+            getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(0.1D);
+            getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(0.5D);
+        }
+    }
 }
